783f5df942dc4b8c5ac55f7e87acbf9346ef16df
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-02-22 10:45:14 -0300

    2/22/24 10:45 AM

---
+++ Exercises/Class_1/Class_1.js
@@ -0,0 +1,19 @@
+// Exercises Class One
+
+// Exercise one
+// 1. Ohm's law [Ley de ohm]
+
+
+const OhmLaw = (obj) => {
+    // La funcion recibe 1 objeto con 2 propiedades: EJ: obj = { prop : 32,4 , prop2 : 12}
+    // La funcion debe funcionar como la ley de OHM que dice lo siguiente
+    // V = I * R || I = V / R || R = V / I (./images/ley_ohm.png)
+    // V = Voltage - R = Resistencia - I = Amperaje
+    // Primero: La funcion debe descubrir que 2 valores tenemos , de acuerdo a los nombres de las pripiedades
+    // si tenes 'V' o 'R' o 'I'
+    // Luego la funcion debe aplicar la ley de Ohm para obtener el valor faltante.
+    // Por ultimo la funcion debe retornar el nuevo valor
+    // EJ: obj = { V : 45, I : 10} -  R = V / I -  R = 45 / 10  - R = 4.5
+    // Tu codigo AQUI:
+    
+}
