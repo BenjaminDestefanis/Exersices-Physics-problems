c2c7d36945084f3c25e42006ee294d26c7957244
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-02-28 14:05:10 -0300

    2/28/24 2:05 PM

--- Exercises/Class_1/test/class1.test.js
+++ Exercises/Class_1/test/class1.test.js
@@ -18,6 +18,10 @@ describe("1. ohmLaw(obj)", () => {
 
         const case1 = {V: 12, R: 103} // 12 Volts - 103 Ohms
         const case2 = {R: 204, I: 5} // 204 Ohms - 5 Ampers
-        expect(ohmLaw(case1)).toBe()
+        const case3 = {I: 10, V: 24} // 10 Ampers - 24 Volts
+
+        expect(ohmLaw(case1)).toBe(0.11)
+        expect(ohmLaw(case2)).toBe(1.020)
+        expect(ohmLaw(case3)).toBe(2.4)
     })
 })
